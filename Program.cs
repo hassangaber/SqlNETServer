05068c0d9b5f57837121539279b2793a8211fe4e
Hassan Gaber <36974406+hassangaber@users.noreply.github.com>
2021-05-25 01:32:12 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace SqlServerSample
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
             builder.DataSource = "localhost";   
             builder.UserID = "sa";             
-            builder.Password = "Incorrect475";   
+            builder.Password = "Password";   
             builder.InitialCatalog = "master";
 
             // Entering demo mode in program
